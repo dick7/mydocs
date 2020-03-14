00bc71e6ca6bf07f7fcef187bccc4f21a2cb67f7
dick7 <qichangyu123456789@163.com>
2020-03-14 15:39:26 +0800

    dev.sh

---
+++ dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 0. kill mkdocs
+ps -ef|grep mkdocs|awk '{print $2}'|xargs kill -9
+# 1.启动虚拟环境
+cd /home/dick7/GitHub/mydocs
+source venv/bin/activate
+# 2.切换项目目录
+cd mydocs
+# 3.运行服务程序
+mkdocs serve --dev-addr 0.0.0.0:8000
